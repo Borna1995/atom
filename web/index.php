1cdef90c02ceedf1b7679a4cdc9ea08cbeb45be2
borna1995 <borna@plavipixel.hr>
2017-10-09 16:11:34 +0200

    V 1.2

--- web/index.php
+++ web/index.php
@@ -1,1 +1,28 @@
-g
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="/css/style.min.css">
+<title>Title of the document</title>
+</head>
+
+  <body>
+
+    <div class="header">
+      <?php include 'header.html';?>
+    </div>
+
+    <div class="main">
+      <?php include 'main.html';?>
+    </div>
+
+    <div class="sidebar">
+      <?php include 'sidebar.html';?>
+    </div>
+
+    <div class="footer">
+      <?php include 'footer.html';?>
+    </div>
+
+  </body>
+
+</html>
